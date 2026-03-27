ba8eeed5156705aef24f59024e1bfb73f20ebf3d
Success Aderibigbe <success.aderibigbe@epita.fr>
2026-03-27 21:47:27 +0100

    feat: implemented terminal sort

---
+++ terminal_sort.py
@@ -0,0 +1,100 @@
+import time
+import random
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def update_screen(arr, algo):
+    global vis
+    if vis:
+        clear_screen()
+        to_show = []
+        print("> Algorithm  : {}".format(algo))
+        print('********************************************')
+        for i in arr:
+            if int(i) < 10:
+                to_show.append('[{}]  : {}'.format(i, '#'*i))    
+            else:
+                to_show.append('[{}] : {}'.format(i, '#'*i))
+        for i in to_show:
+            print(i)
+        print('********************************************')
+        time.sleep(0.10)  
+
+def compare_screen(arr, algo, j, k):
+    global vis
+    if vis:
+        clear_screen()
+        to_show = []
+        print("> Algorithm  : {}".format(algo))
+        print('********************************************')
+        for i in arr:
+            # Element to be compared
+            if i == j or i == k:
+                if int(i) < 10:
+                    to_show.append('[{}]  : \033[33m{}\033[00m'.format(i, '#'*i))    
+                else:
+                    to_show.append('[{}] : \033[33m{}\033[00m'.format(i, '#'*i))
+            else:
+                if int(i) < 10:
+                    to_show.append('[{}]  : {}'.format(i, '#'*i))    
+                else:
+                    to_show.append('[{}] : {}'.format(i, '#'*i))
+        for i in to_show:
+            print(i)
+        print('********************************************')
+        time.sleep(0.25)
+
+def swap_screen(arr, algo, j, k):
+    global vis
+    if vis:
+        clear_screen()
+        to_show = []
+        print("> Algorithm  : {}".format(algo))
+        print('********************************************')
+        for i in arr:
+            # Element to be compared
+            if i == j or i == k:
+                if int(i) < 10:
+                    to_show.append('[{}]  : \033[34m{}\033[00m'.format(i, '#'*i))    
+                else:
+                    to_show.append('[{}] : \033[34m{}\033[00m'.format(i, '#'*i))
+            else:
+                if int(i) < 10:
+                    to_show.append('[{}]  : {}'.format(i, '#'*i))    
+                else:
+                    to_show.append('[{}] : {}'.format(i, '#'*i))
+        for i in to_show:
+            print(i)
+        print('********************************************')
+        time.sleep(0.25)
+
+def bubble_sort(arr): 
+    n = len(arr) 
+    for i in range(n): 
+        for j in range(0, n-i-1):
+            compare_screen(arr, "Bubble Sort", arr[j], arr[j+1])
+            if arr[j] > arr[j+1]: 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swap_screen(arr, "Bubble Sort", arr[j], arr[j+1])
+    return (arr, "Bubble Sort")
+
+def shuffle(arr):
+    for i in range(len(arr)):
+        r = random.randint(0, len(arr)-1)
+        arr[i], arr[r] = arr[r], arr[i]
+    return arr
+
+def genArr(n):
+    arr = [i for i in range(1, n+1)]
+    arr = shuffle(arr)
+    return arr
+
+vis = True
+# Determines number of rows supported by the open window
+row = os.get_terminal_size()[1]
+
+
+if __name__ == "__main__":
+    bubble_sort([2, 4, 6, 7, 2, 3, 4, 6])
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-import main
+import terminal_sort
 import pytest
 
 
@@ -8,20 +8,20 @@ def sample_numbers() -> list[int]:
 
 
 def test_bubble_sort_orders_numbers(sample_numbers: list[int]) -> None:
-    result = main.bubble_sort(sample_numbers, delay=0, show_steps=False)
+    result = terminal_sort.bubble_sort(sample_numbers, delay=0, show_steps=False)
     assert result == [1, 2, 4, 5, 8]
 
 
 def test_bubble_sort_handles_empty_and_singleton_lists() -> None:
-    assert main.bubble_sort([], delay=0, show_steps=False) == []
-    assert main.bubble_sort([7], delay=0, show_steps=False) == [7]
+    assert terminal_sort.bubble_sort([], delay=0, show_steps=False) == []
+    assert terminal_sort.bubble_sort([7], delay=0, show_steps=False) == [7]
 
 
 def test_bubble_sort_keeps_in_place_behavior() -> None:
     values = [3, 1, 2]
     original_id = id(values)
 
-    result = main.bubble_sort(values, delay=0, show_steps=False)
+    result = terminal_sort.bubble_sort(values, delay=0, show_steps=False)
 
     assert id(result) == original_id
     assert values == [1, 2, 3]
@@ -35,9 +35,9 @@ def test_bubble_sort_does_not_sleep_when_delay_is_zero(
     def fake_sleep(_: float) -> None:
         calls["count"] += 1
 
-    monkeypatch.setattr(main.time, "sleep", fake_sleep)
+    monkeypatch.setattr(terminal_sort.time, "sleep", fake_sleep)
 
-    main.bubble_sort([4, 1, 3], delay=0, show_steps=True)
+    terminal_sort.bubble_sort([4, 1, 3], delay=0, show_steps=True)
 
     assert calls["count"] == 0
 
@@ -45,7 +45,7 @@ def test_bubble_sort_does_not_sleep_when_delay_is_zero(
 def test_bubble_sort_can_disable_step_output(
     capsys: pytest.CaptureFixture[str],
 ) -> None:
-    main.bubble_sort([4, 2, 1], delay=0, show_steps=False)
+    terminal_sort.bubble_sort([4, 2, 1], delay=0, show_steps=False)
 
     captured = capsys.readouterr()
     assert captured.out == ""
